7a3892868521e4b2b1e5ff6b036e9cf336a797dc
Phil Howard <phil@gadgetoid.com>
2018-10-03 11:33:47 +0100

    Namespace support

--- library/inky/__init__.py
+++ library/inky/__init__.py
@@ -3,3 +3,10 @@ from .phat import InkyPHAT
 from .what import InkyWHAT
 
 __version__ = '0.0.1'
+
+try:
+    from pkg_resources import declare_namespace
+    declare_namespace(__name__)
+except ImportError:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
