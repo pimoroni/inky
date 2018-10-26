50196c096a5a3d5094a7382939d131b7995492f9
sandyjmacdonald <sandyjmacdonald@gmail.com>
2018-10-26 12:28:07 +0000

    Removing old fonts bits

--- fonts/inky/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from pkg_resources import declare_namespace
-    declare_namespace(__name__)
-except ImportError:
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
