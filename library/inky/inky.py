ee6cbc1fbb8caf6c8b5bee2ef3676775ec978311
Phil Howard <phil@gadgetoid.com>
2019-03-07 13:09:39 +0000

    Reformatted and documented LUTs

--- library/inky/inky.py
+++ library/inky/inky.py
@@ -92,82 +92,111 @@ class Inky:
 
         self._gpio_setup = False
 
+        """Inky Lookup Tables.
+
+        These lookup tables comprise of two sets of values.
+
+        The first set of values, formatted as binary, describe the voltages applied during the six update phases:
+
+          Phase 0     Phase 1     Phase 2     Phase 3     Phase 4     Phase 5     Phase 6
+          A B C D
+        0b01001000, 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,  LUT0 - Black
+        0b01001000, 0b10100000, 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,  LUT1 - White
+        0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  NOT USED BY HARDWARE
+        0b01001000, 0b10100101, 0b00000000, 0b10111011, 0b00000000, 0b00000000, 0b00000000,  LUT3 - Yellow or Red
+        0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  LUT4 - VCOM
+
+        There are seven possible phases, arranged horizontally, and only the phases with duration/repeat information
+        (see below) are used during the update cycle.
+
+        Each phase has four steps: A, B, C and D. Each step is represented by two binary bits and these bits can
+        have one of four possible values representing the voltages to be applied. The default values follow:
+
+        0b00: VSS or Ground
+        0b01: VSH1 or 15V
+        0b10: VSL or -15V
+        0b11: VSH2 or 5.4V
+
+        During each phase the Black, White and Yellow (or Red) stages are applied in turn, creating a voltage
+        differential across each display pixel. This is what moves the physical ink particles in their suspension.
+
+        The second set of values, formatted as hex, describe the duration of each step in a phase, and the number
+        of times that phase should be repeated:
+
+          Duration                Repeat
+          A     B     C     D
+        0x10, 0x04, 0x04, 0x04, 0x04,  <-- Timings for Phase 0
+        0x10, 0x04, 0x04, 0x04, 0x04,  <-- Timings for Phase 1
+        0x04, 0x08, 0x08, 0x10, 0x10,      etc
+        0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00,
+
+        The duration and repeat parameters allow you to take a single sequence of A, B, C and D voltage values and
+        transform them into a waveform that - effectively - wiggles the ink particles into the desired position.
+
+        In all of our LUT definitions we use the first and second phases to flash/pulse and clear the display to
+        mitigate image retention. The flashing effect is actually the ink particles being moved from the bottom to
+        the top of the display repeatedly in an attempt to reset them back into a sensible resting position.
+
+        """
         self._luts = {
             'black': [
-            # Phase 0     Phase 1     Phase 2     Phase 3     Phase 4     Phase 5     Phase 6
-            # A B C D     A B C D     A B C D     A B C D     A B C D     A B C D     A B C D
-            0b01001000, 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,  # LUT0 - Black
-            0b01001000, 0b10100000, 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,  # LUTT1 - White
-            0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  # IGNORE
-            0b01001000, 0b10100101, 0b00000000, 0b10111011, 0b00000000, 0b00000000, 0b00000000,  # LUT3 - Red
-            0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  # LUT4 - VCOM
-
-            # Duration            |  Repeat
-            # A   B     C     D   |
-            16,   4,    4,    4,     4,   # 0 Flash
-            16,   4,    4,    4,     4,   # 1 clear
-            4,    8,    8,    16,    16,  # 2 bring in the black
-            0,    0,    0,    0,     0,   # 3 time for red
-            0,    0,    0,    0,     0,   # 4 final black sharpen phase
-            0,    0,    0,    0,     0,   # 5
-            0,    0,    0,    0,     0,   # 6
+                0b01001000, 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,
+                0b01001000, 0b10100000, 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,
+                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0b01001000, 0b10100101, 0b00000000, 0b10111011, 0b00000000, 0b00000000, 0b00000000,
+                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0x10, 0x04, 0x04, 0x04, 0x04,
+                0x10, 0x04, 0x04, 0x04, 0x04,
+                0x04, 0x08, 0x08, 0x10, 0x10,
+                0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00,
             ],
             'red': [
-            # Phase 0     Phase 1     Phase 2     Phase 3     Phase 4     Phase 5     Phase 6
-            # A B C D     A B C D     A B C D     A B C D     A B C D     A B C D     A B C D
-            0b01001000, 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,  # LUT0 - Black
-            0b01001000, 0b10100000, 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,  # LUTT1 - White
-            0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  # IGNORE
-            0b01001000, 0b10100101, 0b00000000, 0b10111011, 0b00000000, 0b00000000, 0b00000000,  # LUT3 - Red
-            0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  # LUT4 - VCOM
-
-            # Duration            |  Repeat
-            # A   B     C     D   |
-            64,   12,   32,   12,    6,   # 0 Flash
-            16,   8,    4,    4,     6,   # 1 clear
-            4,    8,    8,    16,    16,  # 2 bring in the black
-            2,    2,    2,    64,    32,  # 3 time for red
-            2,    2,    2,    2,     2,   # 4 final black sharpen phase
-            0,    0,    0,    0,     0,   # 5
-            0,    0,    0,    0,     0    # 6
+                0b01001000, 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,
+                0b01001000, 0b10100000, 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,
+                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0b01001000, 0b10100101, 0b00000000, 0b10111011, 0b00000000, 0b00000000, 0b00000000,
+                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0x40, 0x0C, 0x20, 0x0C, 0x06,
+                0x10, 0x08, 0x04, 0x04, 0x06,
+                0x04, 0x08, 0x08, 0x10, 0x10,
+                0x02, 0x02, 0x02, 0x40, 0x20,
+                0x02, 0x02, 0x02, 0x02, 0x02,
+                0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00
             ],
             'red_ht': [
-            # Phase 0     Phase 1     Phase 2     Phase 3     Phase 4     Phase 5     Phase 6
-            # A B C D     A B C D     A B C D     A B C D     A B C D     A B C D     A B C D
-            0b01001000, 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,  # LUT0 - Black
-            0b01001000, 0b10100000, 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,  # LUTT1 - White
-            0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  # IGNORE
-            0b01001000, 0b10100101, 0b00000000, 0b10111011, 0b00000000, 0b00000000, 0b00000000,  # LUT3 - Red
-            0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  # LUT4 - VCOM
-
-            # Duration            |  Repeat
-            # A   B     C     D   |
-            67,   10,   31,   10,    4,   # 0 Flash
-            16,   8,    4,    4,     6,   # 1 clear
-            4,    8,    8,    16,    16,  # 2 bring in the black
-            2,    4,    4,    64,    32,  # 3 time for red
-            6,    6,    6,    2,     2,   # 4 final black sharpen phase
-            0,    0,    0,    0,     0,   # 5
-            0,    0,    0,    0,     0    # 6
+                0b01001000, 0b10100000, 0b00010000, 0b00010000, 0b00010011, 0b00000000, 0b00000000,
+                0b01001000, 0b10100000, 0b10000000, 0b00000000, 0b00000011, 0b00000000, 0b00000000,
+                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0b01001000, 0b10100101, 0b00000000, 0b10111011, 0b00000000, 0b00000000, 0b00000000,
+                0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0x43, 0x0A, 0x1F, 0x0A, 0x04,
+                0x10, 0x08, 0x04, 0x04, 0x06,
+                0x04, 0x08, 0x08, 0x10, 0x10,
+                0x02, 0x04, 0x04, 0x40, 0x20,
+                0x06, 0x06, 0x06, 0x02, 0x02,
+                0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00
             ],
             'yellow': [
-            # Phase 0     Phase 1     Phase 2     Phase 3     Phase 4     Phase 5     Phase 6
-            # A B C D     A B C D     A B C D     A B C D     A B C D     A B C D     A B C D
-            0b11111010, 0b10010100, 0b10001100, 0b11000000, 0b11010000,  0b00000000, 0b00000000,  # LUT0 - Black
-            0b11111010, 0b10010100, 0b00101100, 0b10000000, 0b11100000,  0b00000000, 0b00000000,  # LUTT1 - White
-            0b11111010, 0b00000000, 0b00000000, 0b00000000, 0b00000000,  0b00000000, 0b00000000,  # IGNORE
-            0b11111010, 0b10010100, 0b11111000, 0b10000000, 0b01010000,  0b00000000, 0b11001100,  # LUT3 - Yellow (or Red)
-            0b10111111, 0b01011000, 0b11111100, 0b10000000, 0b11010000,  0b00000000, 0b00010001,  # LUT4 - VCOM
-
-            # Duration            | Repeat
-            # A   B     C     D   |
-            64,   16,   64,   16,   8,
-            8,    16,   4,    4,    16,
-            8,    8,    3,    8,    32,
-            8,    4,    0,    0,    16,
-            16,   8,    8,    0,    32,
-            0,    0,    0,    0,    0,
-            0,    0,    0,    0,    0,
+                0b11111010, 0b10010100, 0b10001100, 0b11000000, 0b11010000, 0b00000000, 0b00000000,
+                0b11111010, 0b10010100, 0b00101100, 0b10000000, 0b11100000, 0b00000000, 0b00000000,
+                0b11111010, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0b11111010, 0b10010100, 0b11111000, 0b10000000, 0b01010000, 0b00000000, 0b11001100,
+                0b10111111, 0b01011000, 0b11111100, 0b10000000, 0b11010000, 0b00000000, 0b00010001,
+                0x40, 0x10, 0x40, 0x10, 0x08,
+                0x08, 0x10, 0x04, 0x04, 0x10,
+                0x08, 0x08, 0x03, 0x08, 0x20,
+                0x08, 0x04, 0x00, 0x00, 0x10,
+                0x10, 0x08, 0x08, 0x00, 0x20,
+                0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00, 0x00,
             ]
         }
 
