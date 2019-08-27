26a45e042d9f0878c4350adb5f5f658b80276c84
daviehh <25255906+daviehh@users.noreply.github.com>
2019-08-27 18:24:04 -0400

    drop matplotlib, draw directly with tk

--- library/inky/mock.py
+++ library/inky/mock.py
@@ -13,13 +13,14 @@ class InkyMock(inky.Inky):
         :param colour: one of red, black or yellow, default: black
 
         """
-        global matplotlib, pyplot
+        global tkinter, ttk, ImageTk, Image
 
         try:
-            import matplotlib
-            from matplotlib import pyplot
+            import tkinter
+            from tkinter import ttk
+            from PIL import ImageTk, Image
         except ImportError:
-            sys.exit('Simulation requires the matplotlib package\nInstall with: pip install matplotlib')
+            sys.exit('Simulation requires tkinter')
 
         resolution = (self.WIDTH, self.HEIGHT)
 
@@ -38,17 +39,37 @@ class InkyMock(inky.Inky):
         self.h_flip = h_flip
         self.v_flip = v_flip
 
-        self._colordicts = {
-            'red': {'red': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 1, 1)),
-                    'green': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0, 0)),
-                    'blue': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0, 0))},
-            'yellow': {'red': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0.73, 0.73)),
-                       'green': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0.62, 0.62)),
-                       'blue': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0, 0))},
-            'black': {'red': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0, 0)),
-                      'green': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0, 0)),
-                      'blue': ((0.0, 1, 1), (0.5, 0, 0), (1.0, 0, 0))}
-        }
+        bw_inky_palette = [255, 255, 255,  # 0 = white
+                              0, 0, 0]  # 1 = black
+        
+        red_inky_palette = [255, 255, 255,  # 0 = white
+                              0, 0, 0,  # 1 = black
+                              255, 0, 0]  # index 2 is red
+        
+        ylw_inky_palette = [255, 255, 255,  # 0 = white
+                              0, 0, 0,  # 1 = black
+                              223, 204, 16]  # index 2 is yellow
+        # yellow color value: screen capture from 
+        # https://www.thoughtsmakethings.com/Pimoroni-Inky-pHAT
+
+        self.c_palette = {"black": bw_inky_palette,
+                          "red": red_inky_palette,
+                          "yellow": ylw_inky_palette}
+
+
+        self.tk_root = tkinter.Tk()
+        self.tk_root.title("Inky Preview")
+        self.tk_root.geometry('{}x{}'.format(self.WIDTH, self.HEIGHT))
+        self.tk_root.aspect(self.WIDTH, self.HEIGHT, self.WIDTH, self.HEIGHT)
+
+    def resize_image(self, event):
+        # https://stackoverflow.com/questions/24061099/tkinter-resize-background-image-to-window-size
+        new_width = event.width
+        new_height = event.height
+        image = self.disp_img_copy.resize((new_width, new_height))
+        photo = ImageTk.PhotoImage(image)
+        self.tk_label.config(image=photo)
+        self.tk_label.image = photo  # avoid garbage collection
 
     def _send_command(self, command, data=None):
         pass
@@ -57,12 +78,16 @@ class InkyMock(inky.Inky):
         pass
 
     def _display(self, region):
-        colormap = matplotlib.colors.LinearSegmentedColormap('einky_colormap', self._colordicts[self.colour], 3)
+        im = Image.fromarray(region, "P")
+        im.putpalette(self.c_palette[self.colour])
 
-        pyplot.figure(figsize=(self.WIDTH / 100.0, self.HEIGHT / 100.0))
-        pyplot.axis('off')
-        pyplot.pcolor(region, cmap=colormap)
-        pyplot.show()
+        self.disp_img_copy = im.copy() # can be changed due to window resizing, so copy
+        photo = ImageTk.PhotoImage(im)
+        self.tk_label = ttk.Label(self.tk_root, image=photo)
+        self.tk_label.bind('<Configure>', self.resize_image)
+        self.tk_label.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+
+        self.tk_root.mainloop()
 
     def show(self, busy_wait=True):
         """Show buffer on display.
@@ -100,8 +125,8 @@ class InkyMockPHAT(InkyMock):
 
     def _simulate(self, region):
         region = numpy.rot90(region, self.rotation // 90)
-        region = numpy.fliplr(region)
-
+        region = numpy.flipud(region)  # spec: phat rotated -90
+        region = numpy.fliplr(region)  # spec: phat rotated -90
         self._display(region)
 
 
@@ -119,7 +144,5 @@ class InkyMockWHAT(InkyMock):
 
     def _simulate(self, region):
         region = numpy.rot90(region, self.rotation // 90)
-        region = region.reshape(300, 400)  # bug? hack?
-        region = numpy.flipud(region)  # bug? hack?
-
+        region = region.reshape(300, 400)  # for display
         self._display(region)
