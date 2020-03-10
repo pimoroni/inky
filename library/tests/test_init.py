70483fd0b88bfd3c642400e7b492b215d16e4286
Phil Howard <phil@gadgetoid.com>
2020-03-10 23:33:19 +0000

    Expand Inky tests to setup

--- library/tests/test_init.py
+++ library/tests/test_init.py
@@ -1,5 +1,7 @@
 """Initialization tests for Inky."""
 
+import mock
+
 
 def test_init_mock_phat_black(tkinter, PIL):
     """Test initialisation of InkyMockPHAT with 'black' colour choice."""
@@ -54,4 +56,37 @@ def test_init_what_yellow(spidev, smbus2):
     """Test initialisation of InkyWHAT with 'yellow' colour choice."""
     from inky import InkyWHAT
 
-    InkyWHAT('red')
+    InkyWHAT('yellow')
+
+
+def test_init_what_setup(spidev, smbus2, GPIO):
+    """Test initialisation and setup of InkyWHAT.
+
+    Verify our expectations for GPIO setup in order to catch regressions.
+
+    """
+    from inky import InkyWHAT
+
+    # TODO: _busy_wait should timeout after N seconds
+    GPIO.input.return_value = GPIO.LOW
+
+    inky = InkyWHAT('red')
+    inky.setup()
+
+    # Check GPIO setup
+    GPIO.setwarnings.assert_called_with(False)
+    GPIO.setmode.assert_called_with(GPIO.BCM)
+    GPIO.setup.assert_has_calls([
+        mock.call(inky.dc_pin, GPIO.OUT, initial=GPIO.LOW, pull_up_down=GPIO.PUD_OFF),
+        mock.call(inky.reset_pin, GPIO.OUT, initial=GPIO.HIGH, pull_up_down=GPIO.PUD_OFF),
+        mock.call(inky.busy_pin, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
+    ])
+
+    # Check device will been reset
+    GPIO.output.assert_has_calls([
+        mock.call(inky.reset_pin, GPIO.LOW),
+        mock.call(inky.reset_pin, GPIO.HIGH)
+    ])
+
+    # Check API will been opened
+    spidev.SpiDev().open.assert_called_with(0, inky.cs_pin)
