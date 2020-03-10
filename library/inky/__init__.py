7815aa6635610ee248f17502d4cc91bc6cfe7f2f
Phil Howard <phil@gadgetoid.com>
2020-03-10 22:12:36 +0000

    Ignore F401 lint error in __init__.py

--- library/inky/__init__.py
+++ library/inky/__init__.py
@@ -1,9 +1,9 @@
 """Inky e-Ink Display Drivers."""
 
-from . import inky
-from .phat import InkyPHAT
-from .what import InkyWHAT
-from .mock import InkyMockPHAT, InkyMockWHAT
+from . import inky  # noqa: F401
+from .phat import InkyPHAT  # noqa: F401
+from .what import InkyWHAT  # noqa: F401
+from .mock import InkyMockPHAT, InkyMockWHAT  # noqa: F401
 
 
 __version__ = '0.0.5.1'
