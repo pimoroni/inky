4b6b8fbc6987b2fe1d347c8ba327b88e589c26aa
Phil Howard <phil@gadgetoid.com>
2020-06-16 20:38:05 +0100

    Add inky2/phat2 driver

--- library/inky/__init__.py
+++ library/inky/__init__.py
@@ -2,8 +2,8 @@
 
 from . import inky  # noqa: F401
 from .inky import BLACK, WHITE, RED, YELLOW  # noqa: F401
-from .phat import InkyPHAT  # noqa: F401
-from .what import InkyWHAT  # noqa: F401
+from .phat import InkyPHAT, InkyPHAT2  # noqa: F401
+from .what import InkyWHAT             # noqa: F401
 from .mock import InkyMockPHAT, InkyMockWHAT  # noqa: F401
 
 
--- library/inky/phat.py
+++ library/inky/phat.py
@@ -9,7 +9,32 @@ The `pinout`_ for the Inky pHAT is documented on pinout.xyz
 .. _`tutorial`: https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat
 .. _`pinout`: https://pinout.xyz/pinout/inky_phat
 """
-from . import inky
+from . import inky, inky2
+
+
+class InkyPHAT2(inky2.Inky):
+    """Inky pHAT V2 (250x122 pixel) e-Ink Display Driver."""
+
+    WIDTH = 250
+    HEIGHT = 122
+
+    WHITE = 0
+    BLACK = 1
+    RED = 2
+    YELLOW = 2
+
+    def __init__(self, colour):
+        """Initialise an Inky pHAT Display.
+
+        :param colour: one of red, black or yellow, default: black
+
+        """
+        inky2.Inky.__init__(
+            self,
+            resolution=(self.WIDTH, self.HEIGHT),
+            colour=colour,
+            h_flip=False,
+            v_flip=False)
 
 
 class InkyPHAT(inky.Inky):
