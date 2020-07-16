36db9a6e358aeae068b43a62f6788f888629fcaa
Phil Howard <phil@gadgetoid.com>
2020-07-16 15:59:13 +0100

    Fix test line endings, fix simulator PIL issue

--- library/tests/test_simulator.py
+++ library/tests/test_simulator.py
@@ -6,49 +6,58 @@ They don't actually validate that our special message is produced!
 
 """
 import pytest
-from inky.mock import InkyMock, InkyMockPHAT, InkyMockWHAT
-
-
-class InkyMockFAIL(InkyMock):
-    """Inky wHAT e-Ink Display Simulator."""
-
-    WIDTH = 999
-    HEIGHT = 999
-
-    WHITE = 0
-    BLACK = 1
-    RED = 2
-    YELLOW = 2
-
-    def _simulate(self, region):
-        pass
 
 
 def test_mock_invalid_size(tkinter, PIL):
     """Test a class with an invalid size raises a ValueError."""
+    from inky.mock import InkyMock
+
+    class InkyMockFAIL(InkyMock):
+        """Inky wHAT e-Ink Display Simulator."""
+
+        WIDTH = 999
+        HEIGHT = 999
+
+        WHITE = 0
+        BLACK = 1
+        RED = 2
+        YELLOW = 2
+
+        def _simulate(self, region):
+            pass
+
+
     with pytest.raises(ValueError):
         InkyMockFAIL('black')
 
 
 def test_mock_invalid_colour(tkinter, PIL):
     """Test that instantiating with an invalid colour raises a ValueError."""
+    from inky.mock import InkyMockPHAT
+
     with pytest.raises(ValueError):
         InkyMockPHAT('octarine')
 
 
 def test_mock_show_phat(tkinter, PIL):
     """Test that show doesn't explode."""
+    from inky.mock import InkyMockPHAT
+
     inky = InkyMockPHAT('red')
     inky.show()
 
 
 def test_mock_show_what(tkinter, PIL):
     """Test that show doesn't explode."""
+    from inky.mock import InkyMockWHAT
+
     inky = InkyMockWHAT('red')
     inky.show()
 
 
 def test_mock_show_180(tkinter, PIL):
     """Test that show doesn't explode."""
+    from inky.mock import InkyMockPHAT
+
     inky = InkyMockPHAT('red', h_flip=True, v_flip=True)
     inky.show()
