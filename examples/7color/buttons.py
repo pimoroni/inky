71a3f7e5a34815339a3c5d08a8a5fc2251564573
Phil Howard <phil@gadgetoid.com>
2020-11-04 14:35:20 +0000

    7-colour examples

---
+++ examples/7color/buttons.py
@@ -0,0 +1,43 @@
+import signal
+import RPi.GPIO as GPIO
+
+print("""buttons.py - Detect which button has been pressed
+
+This example should demonstrate how to:
+ 1. set up RPi.GPIO to read buttons,
+ 2. determine which button has been pressed
+
+Press Ctrl+C to exit!
+
+""")
+
+# Gpio pins for each button (from top to bottom)
+BUTTONS = [5, 6, 16, 24]
+
+# These correspond to buttons A, B, C and D respectively
+LABELS = ['A', 'B', 'C', 'D']
+
+# Set up RPi.GPIO with the "BCM" numbering scheme
+GPIO.setmode(GPIO.BCM)
+
+# Buttons connect to ground when pressed, so we should set them up
+# with a "PULL UP", which weakly pulls the input signal to 3.3V.
+GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+# "handle_button" will be called every time a button is pressed
+# It receives one argument: the associated input pin.
+def handle_button(pin):
+    label = LABELS[BUTTONS.index(pin)]
+    print("Button press detected on pin: {} label: {}".format(pin, label))
+
+
+# Loop through out buttons and attach the "handle_button" function to each
+# We're watching the "FALLING" edge (transition from 3.3V to Ground) and
+# picking a generous bouncetime of 250ms to smooth out button presses.
+for pin in BUTTONS:
+    GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=250)
+
+# Finally, since button handlers don't require a "while True" loop,
+# we pause the script to prevent it exiting immediately.
+signal.pause()
---
+++ examples/7color/stripes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+from inky.inky_uc8159 import Inky
+
+inky = Inky()
+
+for y in range(inky.height - 1):
+    color = y // (inky.height // 7)
+    for x in range(inky.width - 1):
+        inky.set_pixel(x, y, color)
+
+inky.show()
+
