3426bab9d880f73e7f4ab4cde529fc77760e9601
James Conroy <Jconroy12@gmail.com>
2020-11-18 00:11:04 -0600

    Updated the Example Shebangs to Python3
    Changed all the files starting with `#!/bin/env` python to
    instead start with `#!/bin/env python3`.
    
    Also added a section to the readme saying to use python3.

--- examples/identify.py
+++ examples/identify.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from inky.eeprom import read_eeprom
 
 display = read_eeprom()
--- examples/what/dither-image-what.py
+++ examples/what/dither-image-what.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 from PIL import Image
