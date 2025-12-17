48a107ee4c843e6151b2c6231fd783462619882a
thirdr <ryan@pimoroni.com>
2025-12-17 11:55:22 +0000

    E640: updated display variant name

--- inky/eeprom.py
+++ inky/eeprom.py
@@ -35,7 +35,7 @@ DISPLAY_VARIANT = [
     "Spectra 6 7.3 800 x 480 (E673)",
     "Red/Yellow pHAT (JD79661)",
     "Red/Yellow wHAT (JD79668)",
-    "Spectra 6 4.0 400 x 600 (E640)"
+    "Spectra 6 4.0 600 x 400 (E640)"
 ]
 
 
